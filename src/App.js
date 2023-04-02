19aabdcebf1ce6e628aed15ea3051ce602826926
Kirtesh Maskey <kirteshmaske123@gmail.com>
2023-04-03 01:15:21 +0530

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import SetupForm from './Form/SetupForm';
+import Modal from './Modal/Modal';
+import LoadingScreen from './Loading/LoadingScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='quiz'>
+        <p className='correct-answers'>Correct Answers: n</p>
+        <article className='container'>
+          <h2>Text</h2>
+          <div className='btn-container'></div>
+        </article>
+        <button className='next-question'>Next</button>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
